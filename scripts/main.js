f2baddc20e66be7906ee956239f31db1c4e17680
KaraevFan <tom.n.kano@gmail.com>
2025-03-22 21:21:29 +0900

    Update audio source to use Cloudinary URL

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('cinemaButton');
-    const audio = new Audio('assets/sounds/Anime Earth Movement Opening.mp3');
+    const audio = new Audio('https://res.cloudinary.com/dlkzuaqpw/video/upload/v1742645938/Anime_Earth_Movement_Opening_pwt0bj.mp3');
 
     button.addEventListener('click', () => {
         audio.play();
